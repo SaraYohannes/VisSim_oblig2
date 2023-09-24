6e3cca0101235304c37ac2cc9527cc4c69f68f2f
SaraYohannes <89453142+SaraYohannes@users.noreply.github.com>
2023-09-24 16:35:56 +0200

    update

--- Assets/Scripts_VisSimOblig2/Barycentric.cs
+++ Assets/Scripts_VisSimOblig2/Barycentric.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Barycentric : MonoBehaviour
 {
@@ -24,96 +26,62 @@ public class Barycentric : MonoBehaviour
         tester_gizmo = tester_gizmo.GetComponent<Transform>();
 
 
-        Vector3 sphere_vec = SphereInfo.transform.position;
-        Vector3 currentLocaton = baryCentricPosition(meshinfo, sphere_vec);
-        tester_gizmo.transform.position = currentLocaton;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        Vector3 sphere_vec = SphereInfo.transform.position;
+        Vector3 currentLocaton = baryCentricPosition(meshinfo, sphere_vec);
+        Debug.Log(currentLocaton);
+        tester_gizmo.transform.position = currentLocaton;
     }
 
     Vector3 baryCentricPosition(Vector3[] vertexInformation, Vector3 sphere)
     {
-        // barycentric calculations
-        Vector3 barycentric;
+        Vector3 p1 = new Vector3(), p2 = new Vector3(), p3 = new Vector3();
+        Vector3 barycentric = new Vector3 (-1.0f, -1.0f, -1.0f);
+        Vector2 vec2sphere = new Vector2(sphere.x, sphere.z);
 
-        Vector2 t_0;
-        t_0.x = vertexInformation[0].x;
-        t_0.y = vertexInformation[0].z;
-        Vector2 t_1;
-        t_1.x = vertexInformation[1].x;
-        t_1.y = vertexInformation[1].z;
-        Vector2 t_2;
-        t_2.x = vertexInformation[2].x;
-        t_2.y = vertexInformation[2].z;
-        Vector2 t_p;
-        t_p.x = sphere.x;
-        t_p.y = sphere.z;
-        
-        Vector2 _0P = t_p - t_0;
-        Vector2 _1P = t_p + t_1;
-        Vector2 _2P = t_p - t_2;
+        for (int i = 0; i < vertexInformation.Length / 3; i++)
+        {
+            p1 = vertexInformation[i * 3];
+            p2 = vertexInformation[i * 3 + 1];
+            p3 = vertexInformation[i * 3 + 2];
 
-        float mortal =  Vector2.Dot((t_1-t_0), (t_2-t_0));
+            barycentric = getBaryC(new Vector2(p1.x, p1.z), new Vector2(p2.x, p2.z), new Vector2(p3.x, p3.z), vec2sphere);
 
-        float u = ((_0P.x*_1P.y)-(_0P.y*_1P.x)) / mortal;
-        float v = ((_1P.x * _2P.y) - (_1P.y * _2P.x)) / mortal;
-        float w = ((_2P.x * _0P.y) - (_2P.y * _0P.x)) / mortal;
-
-        float result = u + v + w;
-
-        barycentric.x = u; barycentric.z = v; barycentric.y = w;    
-         
-        Debug.Log(result);
-       
-        
-        /*
-        Vector3 u = vertexInformation[1] - vertexInformation[0];
-        Vector3 v = vertexInformation[2] - vertexInformation[0];
-        Vector3 w = sphere - vertexInformation[0];
-
-        // cross products
-        Vector3 uw;
-        uw.x = (u.z * w.y) - (u.y * w.z);
-        uw.z = (u.x * w.y) - (u.z * w.x);
-        uw.y = (u.x * w.z) - (u.z * w.x);
-        float normal = 
-
-        Vector3 vw;
-        uw.x = (v.z * w.y) - (v.y * w.z);
-        uw.z = (v.x * w.y) - (v.z * w.x);
-        uw.y = (v.x * w.z) - (v.z * w.x);
-        */
-
-
-        /*
-        // first define triangle and its normal
-        Vector3 A = vertexInformation[0];
-        Vector3 B = vertexInformation[1];
-        Vector3 C = vertexInformation[2];
-
-        float triangle_normal = Vector3.Dot(C - A, B - A);        
-
-        float d_ABAB = Vector3.Dot(A, B) * Vector3.Dot(A, B);
-        float d_ABAC = Vector3.Dot(A, B) * Vector3.Dot(A, C);
-        float d_ACAC = Vector3.Dot(A, C) * Vector3.Dot(A, C);
-        float d_APAB = Vector3.Dot(A, sphere) * Vector3.Dot(A, B);
-        float d_APAC = Vector3.Dot(A, sphere) * Vector3.Dot(A, C);
-
-        float u = (d_ACAC * d_APAB) - (d_ABAC * d_APAC) / triangle_normal;
-        float v = (d_ABAB * d_APAC) - (d_ABAC * d_APAB) / triangle_normal;
-        float w = 1.0f - u - v;
-
-        Debug.Log("Barycentric Coordinates: " + "( " + u + ", " + v + ", " + w + " )");
-        float result = u + v + w;
-
-        Debug.Log("BaryCentric result: " + result);
-        */
+            if (barycentric.x >= 0 && barycentric.y >= 0 && barycentric.z >= 0)
+            {
+                break;
+            }
+        }
 
         return barycentric;
         
     }
 
+    Vector3 getBaryC(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 vec2sphere)
+    {
+        Vector2 v0 = p2 - p1;
+        Vector2 v1 = p3 - p1;
+        Vector2 v2 = vec2sphere - p1;
+        Debug.Log("GetBaryC v0: " + v0 + " v1: " + v1 + " v2: " + v2);
+        float d00 = Vector2.Dot(v0, v0);
+        float d01 = Vector2.Dot(v0, v1);
+        float d11 = Vector2.Dot(v1, v1);
+        float d20 = Vector2.Dot(v2, v0);
+        float d21 = Vector2.Dot(v2, v1);
+        float denom = d00 * d11 - d01 * d01;
+        Debug.Log("GetBaryC denom: " + denom);
+        float v = (d11 * d20 - d01 * d21) / denom;
+        float w = (d00 * d21 - d01 * d20) / denom;
+        float u = (1.0f - v - w);
+
+        Vector3 barycentric3D = new Vector3(u, v, w);
+        Debug.Log("GetBaryC returns: " + barycentric3D);
+
+        return barycentric3D;
+
+    }
 
 }
